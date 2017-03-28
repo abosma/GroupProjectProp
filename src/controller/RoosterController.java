05a05dff33300377bc9275ca5f11addea247fcef
abosma <atillabosma@hotmail.com>
2017-03-28 14:23:20 +0200

    Documentatie RoosterController.java

--- src/controller/RoosterController.java
+++ src/controller/RoosterController.java
@@ -30,17 +30,21 @@ public class RoosterController {
 	private void ophalenDocent(Conversation conversation) {
 		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
 		String lGebruikersnaam = lJsonObjectIn.getString("username");
+		//Geeft alle lessen terug van docent met username van lJsonObjectIn
 		ArrayList<Les> lessenDocent = informatieSysteem.getLessenDocent(lGebruikersnaam);
 		
 		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
 		
 		for (Les l : lessenDocent) {
+			//Dit is om het begin en eindtijd op te halen
 			ArrayList<LocalDateTime> tempArray = l.getLesData();
 			
+			//0 is begintijd, 1 is eindtijd in l.getLesData()
 			LocalDateTime beginData = tempArray.get(0);
 			LocalDateTime eindData = tempArray.get(1);
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
       
+			//Hier wordt de LocalDateTime naar een string omgezet, dus van 01-01-2000T00:00:00 naar 2000-01-01 00:00:00
 			String beginDataString = beginData.format(formatter);
       String eindDataString = eindData.format(formatter);
 			String lesNaam = l.getNaam();
@@ -48,6 +52,7 @@ public class RoosterController {
 			String lokaal = l.getLokaal();
 			String klasNaam = l.getKlas().getNaam();
 			
+			//Hier voeg ik alle strings toe aan de JsonObjectBuilder zodat Polymer alles kan gebruiken
 			JsonObjectBuilder lJsonObjectBuilderVoorDocent = Json.createObjectBuilder();
 			lJsonObjectBuilderVoorDocent
 				.add("beginData", beginDataString)
@@ -64,8 +69,9 @@ public class RoosterController {
 	}
 
 	private void ophalenStudent(Conversation conversation) {
+		//Hetzelfde als ophalenDocent behalve dat het getLessenStudent gebruikt in plaats van getLessenDocent
 		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
-		String lKlasNaam = lJsonObjectIn.getString("klasnaam");
+		String lKlasNaam = lJsonObjectIn.getString("username");
 		ArrayList<Les> lessenStudent = informatieSysteem.getLessenStudent(lKlasNaam);
 		
 		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
