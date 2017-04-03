ab4ca727f9c7de1d3fb516b45d8e3d764da7a0a0
ronaldderooy <ronnieflexican@gmail.com>
2017-04-03 21:21:41 +0200

    Presentiecontroller gegevens bijna af

--- src/controller/PresentieController.java
+++ src/controller/PresentieController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import javax.json.Json;
 
+
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -21,6 +22,7 @@ import server.Handler;
 
 public class PresentieController implements Handler   {
 	private PrIS informatieSysteem;
+ 
 
 	public PresentieController(PrIS infoSys) {
 		informatieSysteem = infoSys;
@@ -36,59 +38,97 @@ public class PresentieController implements Handler   {
 	
 	private void studentOphalen(Conversation conversation)  {
 		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+		
 		String lGebruikersnaam = lJsonObjectIn.getString("username");
 		ArrayList<Les> lessenStudent = informatieSysteem.getLessenStudent(lGebruikersnaam);
-		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+
+//		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+		
+//		JsonArrayBuilder lJsonArrayAui = Json.createArrayBuilder();
+		
+		ArrayList<String> vakken = new ArrayList<String>();
+//		ArrayList<String> V1AUI = new ArrayList<String>();
+		
+//		JsonObjectBuilder lJsonObjectBuilderVoorAuiLijst = Json.createObjectBuilder();
+		
+		
+		//De verschillende vakken in een lijst zetten
 		
 		for (Les l : lessenStudent) {
-			JsonObjectBuilder lJsonObjectBuilderVoorStudent = Json.createObjectBuilder();
-			JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
+			if(vakken.isEmpty()){vakken.add(l.getNaam()); ;}
 			
-			
-			LocalDateTime beginDatum = l.getBeginDatum();
-			LocalDateTime eindDatum = l.getEindDatum();
-			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	    
-			String beginDatumString = beginDatum.format(formatter);
-	    String eindDatumString = eindDatum.format(formatter);
-			String docentNaam = l.getDocent().getGebruikersnaam();
-			String lokaal = l.getLokaal();
-			
-			lJsonObjectBuilderVoorStudent
-				.add(l.getNaam(), lJsonObjectBuilderVoorLes)
-  				.add("beginDatum", beginDatumString)
-  				.add("eindDatum", eindDatumString)
-  				.add("docentNaam", docentNaam)
-  				.add("lokaal", lokaal);
-			for(Map.Entry<String, Integer> pres : l.getPresentie().entrySet()){
-    		if(pres.getKey().equals(lGebruikersnaam)){
-    			String s = null;
-    			switch(pres.getValue()){
-    				case 0:
-    					s = "Afwezig";
-    					break;
-    				case 1:
-    					s = "Aanwezig";
-    					break;
-    				case 2:
-    					s = "Ziek";
-    					break;
-    				case 3:
-    					s = "Afgemeld";
-    					break;
-    				case 4:
-    					s = "Afgemeld (Niet geaccepteerd)";
-    					break;
-    			}
-    			lJsonObjectBuilderVoorLes.add(pres.getKey(), s);
-    		}
-    	}
-		  lJsonArrayBuilder.add(lJsonObjectBuilderVoorStudent);
+			else if(!vakken.contains(l.getNaam())) {
+					vakken.add(l.getNaam());
+			}
 		}
-    String lJsonOutStr = lJsonArrayBuilder.build().toString();		
-    
-		conversation.sendJSONMessage(lJsonOutStr);
+		JsonObjectBuilder lJsonObjectBuilderVoorPresentie = Json.createObjectBuilder();
+		for(String vak: vakken){
+			int aui = 0;
+			//Key die het hele JSON bestand in een lijst heeft staan
+			
+			//Een naam van het vak die een lijst bevat met de lesgegevens
+			JsonObjectBuilder lJsonObjectBuilderVoorVak = Json.createObjectBuilder();
+			
+			//lesnummer(als string bijv:les1) en de aanwezigheid als string
+			JsonArrayBuilder lJsonArrayBuilderVoorLes = Json.createArrayBuilder();
+//			System.out.println(lJsonArrayBuilderVoorLes);
+			
+			for (Les l : lessenStudent) {
+				JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
+				LocalDateTime beginDatum = l.getBeginData();
+				LocalDateTime eindDatum = l.getEindData();
+				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		    
+				String beginDatumString = beginDatum.format(formatter);
+		    String eindDatumString = eindDatum.format(formatter);
+				String docentNaam = l.getDocent().getGebruikersnaam();
+				String lokaal = l.getLokaal();
+				
+				for(Map.Entry<String, Integer> pres : l.getPresentie().entrySet()){
+	  		if(pres.getKey().equals(lGebruikersnaam)){
+	  			String s = null;
+	  			switch(pres.getValue()){
+	  				case 0:
+	  					s = "Afwezig";
+	  					break;
+	  				case 1:
+	  					s = "Aanwezig";
+	  					break;
+	  				case 2:
+	  					s = "Ziek";
+	  					break;
+	  				case 3:
+	  					s = "Afgemeld";
+	  					break;
+	  				case 4:
+	  					s = "Afgemeld (Niet geaccepteerd)";
+	  					break;
+	  			}
+	  			if(l.getNaam().equals(vak)){
+
+	  				aui = aui+1;
+  					String lesnummer = "Les "+ String.valueOf(aui);
+	  				lJsonObjectBuilderVoorLes.add(lesnummer, s);
+	  				lJsonArrayBuilderVoorLes.add(lJsonObjectBuilderVoorLes);
+	  			}
+	  	}
+	  }
 	}
+			
+	
+			
+			lJsonObjectBuilderVoorVak.add(vak, lJsonArrayBuilderVoorLes);
+			//hier toevoegen aan json voor de ene vak
+			
+			
+			String lJsonOutStr = lJsonObjectBuilderVoorVak.build().toString();	
+			
+			System.out.println(lJsonOutStr);
+			}
+		
+	}
+	
+	
 	
 	private void docentOphalen(Conversation conversation)  {
 	JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
@@ -103,8 +143,8 @@ public class PresentieController implements Handler   {
 		for (Les l : lessenStudent) {
 			JsonObjectBuilder lJsonObjectBuilderVoorStudent = Json.createObjectBuilder();
 			
-			LocalDateTime beginDatum = l.getBeginDatum();
-			LocalDateTime eindDatum = l.getEindDatum();
+			LocalDateTime beginDatum = l.getBeginData();
+			LocalDateTime eindDatum = l.getEindData();
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	    
 			String beginDatumString = beginDatum.format(formatter);
@@ -123,6 +163,7 @@ public class PresentieController implements Handler   {
 		  lJsonArrayBuilder.add(lJsonObjectBuilderVoorStudent);
 		}
     String lJsonOutStr = lJsonArrayBuilder.build().toString();		
+//    System.out.println(lJsonOutStr);
     
 		conversation.sendJSONMessage(lJsonOutStr);
 	}
